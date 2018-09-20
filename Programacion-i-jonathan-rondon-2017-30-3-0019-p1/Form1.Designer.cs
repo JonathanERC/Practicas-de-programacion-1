bc230273d122ad024a8e0d602ffb86a905d9d1c1
Jonathan Rondon <jonathanestebanrondon@gmail.com>
2018-09-20 09:51:14 -0400

    Add project files.

---
+++ Programacion-i-jonathan-rondon-2017-30-3-0019-p1/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Programacion_i_jonathan_rondon_2017_30_3_0019_p1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.btncln = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.txtvalor2 = new System.Windows.Forms.TextBox();
+            this.txtvalor1 = new System.Windows.Forms.TextBox();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.txtrai = new System.Windows.Forms.TextBox();
+            this.txtmult = new System.Windows.Forms.TextBox();
+            this.txtpot = new System.Windows.Forms.TextBox();
+            this.txtdiv = new System.Windows.Forms.TextBox();
+            this.txtrest = new System.Windows.Forms.TextBox();
+            this.txtsum = new System.Windows.Forms.TextBox();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label29 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.btncln);
+            this.groupBox7.Controls.Add(this.btncalc);
+            this.groupBox7.Controls.Add(this.txtvalor2);
+            this.groupBox7.Controls.Add(this.txtvalor1);
+            this.groupBox7.Controls.Add(this.label25);
+            this.groupBox7.Controls.Add(this.label26);
+            this.groupBox7.Location = new System.Drawing.Point(7, 18);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(415, 116);
+            this.groupBox7.TabIndex = 0;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Operadores";
+            this.groupBox7.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btncln
+            // 
+            this.btncln.Location = new System.Drawing.Point(311, 71);
+            this.btncln.Name = "btncln";
+            this.btncln.Size = new System.Drawing.Size(75, 23);
+            this.btncln.TabIndex = 2;
+            this.btncln.Text = "Limpiar";
+            this.btncln.UseVisualStyleBackColor = true;
+            this.btncln.Click += new System.EventHandler(this.btncln_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.Location = new System.Drawing.Point(311, 33);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(75, 23);
+            this.btncalc.TabIndex = 2;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = true;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // txtvalor2
+            // 
+            this.txtvalor2.Location = new System.Drawing.Point(90, 74);
+            this.txtvalor2.Name = "txtvalor2";
+            this.txtvalor2.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor2.TabIndex = 1;
+            // 
+            // txtvalor1
+            // 
+            this.txtvalor1.Location = new System.Drawing.Point(90, 33);
+            this.txtvalor1.Name = "txtvalor1";
+            this.txtvalor1.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor1.TabIndex = 1;
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(7, 77);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(79, 13);
+            this.label25.TabIndex = 0;
+            this.label25.Text = "Segundo valor:";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(7, 33);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(65, 13);
+            this.label26.TabIndex = 0;
+            this.label26.Text = "Primer valor:";
+            this.label26.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.Controls.Add(this.txtrai);
+            this.groupBox8.Controls.Add(this.txtmult);
+            this.groupBox8.Controls.Add(this.txtpot);
+            this.groupBox8.Controls.Add(this.txtdiv);
+            this.groupBox8.Controls.Add(this.txtrest);
+            this.groupBox8.Controls.Add(this.txtsum);
+            this.groupBox8.Controls.Add(this.label27);
+            this.groupBox8.Controls.Add(this.label28);
+            this.groupBox8.Controls.Add(this.label29);
+            this.groupBox8.Controls.Add(this.label30);
+            this.groupBox8.Controls.Add(this.label31);
+            this.groupBox8.Controls.Add(this.label32);
+            this.groupBox8.Location = new System.Drawing.Point(7, 140);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(416, 117);
+            this.groupBox8.TabIndex = 1;
+            this.groupBox8.TabStop = false;
+            // 
+            // txtrai
+            // 
+            this.txtrai.Location = new System.Drawing.Point(237, 80);
+            this.txtrai.Name = "txtrai";
+            this.txtrai.Size = new System.Drawing.Size(100, 20);
+            this.txtrai.TabIndex = 1;
+            // 
+            // txtmult
+            // 
+            this.txtmult.Location = new System.Drawing.Point(88, 80);
+            this.txtmult.Name = "txtmult";
+            this.txtmult.Size = new System.Drawing.Size(100, 20);
+            this.txtmult.TabIndex = 1;
+            // 
+            // txtpot
+            // 
+            this.txtpot.Location = new System.Drawing.Point(256, 46);
+            this.txtpot.Name = "txtpot";
+            this.txtpot.Size = new System.Drawing.Size(100, 20);
+            this.txtpot.TabIndex = 1;
+            // 
+            // txtdiv
+            // 
+            this.txtdiv.Location = new System.Drawing.Point(255, 20);
+            this.txtdiv.Name = "txtdiv";
+            this.txtdiv.Size = new System.Drawing.Size(100, 20);
+            this.txtdiv.TabIndex = 1;
+            // 
+            // txtrest
+            // 
+            this.txtrest.Location = new System.Drawing.Point(52, 46);
+            this.txtrest.Name = "txtrest";
+            this.txtrest.Size = new System.Drawing.Size(100, 20);
+            this.txtrest.TabIndex = 1;
+            // 
+            // txtsum
+            // 
+            this.txtsum.Location = new System.Drawing.Point(51, 20);
+            this.txtsum.Name = "txtsum";
+            this.txtsum.Size = new System.Drawing.Size(100, 20);
+            this.txtsum.TabIndex = 1;
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Location = new System.Drawing.Point(198, 87);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(33, 13);
+            this.label27.TabIndex = 0;
+            this.label27.Text = "Raíz:";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Location = new System.Drawing.Point(8, 87);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(74, 13);
+            this.label28.TabIndex = 0;
+            this.label28.Text = "Multiplicación:";
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.Location = new System.Drawing.Point(198, 53);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(52, 13);
+            this.label29.TabIndex = 0;
+            this.label29.Text = "Potencia:";
+            // 
+            // label30
+            // 
+            this.label30.AutoSize = true;
+            this.label30.Location = new System.Drawing.Point(8, 53);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(38, 13);
+            this.label30.TabIndex = 0;
+            this.label30.Text = "Resta:";
+            // 
+            // label31
+            // 
+            this.label31.AutoSize = true;
+            this.label31.Location = new System.Drawing.Point(197, 20);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(47, 13);
+            this.label31.TabIndex = 0;
+            this.label31.Text = "División:";
+            // 
+            // label32
+            // 
+            this.label32.AutoSize = true;
+            this.label32.Location = new System.Drawing.Point(7, 20);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(37, 13);
+            this.label32.TabIndex = 0;
+            this.label32.Text = "Suma:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(440, 286);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox7);
+            this.tabPage1.Controls.Add(this.groupBox8);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(432, 260);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Calculadora";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(432, 260);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Problema";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(440, 285);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Operador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.groupBox8.ResumeLayout(false);
+            this.groupBox8.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Button btncln;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.TextBox txtvalor2;
+        private System.Windows.Forms.TextBox txtvalor1;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.TextBox txtrai;
+        private System.Windows.Forms.TextBox txtmult;
+        private System.Windows.Forms.TextBox txtpot;
+        private System.Windows.Forms.TextBox txtdiv;
+        private System.Windows.Forms.TextBox txtrest;
+        private System.Windows.Forms.TextBox txtsum;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.Label label32;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ Programacion-i-jonathan-rondon-2017-30-3-0019-p1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion_i_jonathan_rondon_2017_30_3_0019_p1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            //declarar variables
+            double num1, num2, sum, rest, mult, div, pot, rai;
+            //entrada de datos
+            num1 = double.Parse(txtvalor1.Text);
+            num2 = Convert.ToDouble(txtvalor2.Text);
+            //proceso
+            sum = num1 + num2;
+            rest = num1 - num2;
+            mult = num1 * num2;
+            div = num1 / num2;
+            pot = Math.Pow(num1, num2);
+            rai = Math.Pow(num1, (1/num2));
+            //salida de info
+            txtsum.Text = Convert.ToString(sum);
+            txtrest.Text = Convert.ToString(rest);
+            txtdiv.Text = Convert.ToString(rest);
+            txtmult.Text = Convert.ToString(mult);
+            txtpot.Text = Convert.ToString(pot);
+            txtrai.Text = Convert.ToString(rai);
+            
+        }
+
+        private void btncln_Click(object sender, EventArgs e)
+        {
+            txtvalor1.Clear();
+            txtvalor2.Clear();
+            txtsum.Clear();
+            txtrest.Clear();
+            txtmult.Clear();
+            txtdiv.Clear();
+            txtpot.Clear();
+            txtrai.Clear();
+        }
+
+        private void btnsumar_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text = Convert.ToString("1");
+        }
+    }
+}
